a32d065085ee014387f2a8d24c726967e3bb5e3a
pkwak1 <pkwak@Pauls-MacBook-Pro.local>
2016-10-01 16:57:51 -0400

    server test

--- server/server.py
+++ server/server.py
@@ -1,5 +1,8 @@
 import socket
 import json
+import os
+from os.path import join, dirname
+from watson_developer_cloud import TradeoffAnalyticsV1
 
 HOST, PORT = '', 8888
 
@@ -13,9 +16,25 @@ while True:
     request = client_connection.recv(1024)
     print request
 
-    with open("dummy.json") as json_file:
+    '''with open("userAnalyticsPy.json") as json_file:
         d = json.load(json_file)
-        print(d)
+        print(d)'''
+
+    tradeoff_analytics = TradeoffAnalyticsV1(
+        username='af7839e5-5719-4357-9f2f-8b4607ba84d5',
+        password='IY2KB0panLuF')
+
+    with open(join(dirname(__file__), 'userAnalyticsPy.json')) as data_file:
+        problem_data = json.load(data_file)
+    # print(json.dumps(tradeoff_analytics.dilemmas(problem_data), indent=2))
+    resolution = tradeoff_analytics.dilemmas(problem_data)
+    solutionNum = 0
+    while resolution["resolution"]["solutions"][solutionNum]["status"]!="FRONT":
+        solutionNum = solutionNum+1
+    firstChoice = resolution["resolution"]["solutions"][solutionNum]["solution_ref"]
+    index = int(firstChoice)-1
+    reccomend = resolution["problem"]["options"][index]["name"]
+    print(reccomend)
 
     http_response = """\
 HTTP/1.1 200 OK
